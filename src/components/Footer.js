616749e3d8f754f1a2073893cffe85b28ff20fa2
usernamedinesh <borod9200@gmail.com>
2025-08-22 15:49:59 +0530

    fix footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 export function FooterPage() {
   const currentYear = new Date().getFullYear();
   return `
-    <footer class="relative  bg-gray-800 text-white p-4 text-center h-11 items-center">
-    <div class="absolute inset-x-0 bottom-2">
+    <footer class="relative  bg-gray-800 text-white text-center h-30 justify-center items-center mt-10">
+    <div class="absolute inset-x-0 bottom-10 text-center ">
           <p>&copy; ${currentYear} My Website. All rights reserved.</p>
     </div>
     </footer>
