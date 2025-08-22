8a6f8264b27c7dde1c673613fbef5bb18bcaa6c9
usernamedinesh <borod9200@gmail.com>
2025-08-22 16:21:35 +0530

    fixing ui of tutorial

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,4 +1,7 @@
 import v1 from "../assets/Vector1.png"
+import download from  "../assets/download.png"
+import paste from  "../assets/paste.png"
+import link from  "../assets/link.png"
 
 export function HowToDownload() {
   return `
@@ -6,9 +9,16 @@ export function HowToDownload() {
       <h1 class="text-3xl font-bold">How to Download Videos?</h1>
 
       <div class="grid grid-rows-3 gap-20 md:flex md:justify-between md:mx-20 mt-10 text-left">
-        
-        <div class="w-full md:w-1/3 bg-amber-50 text-black px-10 py-30 cursor-pointer rounded-3xl transform hover:scale-105 transition duration-300">
-          <h2 class="text-center text-xl font-semibold">Copy the URL</h2>
+
+        <!-- Card 1 -->
+        <div class="relative w-full md:w-1/3 bg-amber-50 text-black px-10 py-30 cursor-pointer rounded-3xl transform hover:scale-105 transition duration-300">
+          <!-- floating icon -->
+      <!-- Floating circle with padding -->
+        <div class="absolute -top-7 left-1/2 -translate-x-1/2 w-14 h-14 bg-lime-300 rounded-full flex items-center justify-center">
+          <img src=${link} alt="Pinterest" class="w-7 h-7" />
+        </div>
+          
+          <h2 class="text-center text-xl font-semibold mt-10">Copy the URL</h2>
           <p class="mt-5 text-base font-normal">
             On Instagram, Facebook, Twitter, or Pinterest, open the post,
             tap the three dots or share icon, and select "Copy Link".
@@ -16,15 +26,25 @@ export function HowToDownload() {
           </p>
         </div>
 
-        <div class="w-full md:w-1/3 bg-amber-50 text-black px-10 rounded-3xl cursor-pointer py-30  transform hover:scale-105 transition duration-300 ">
-          <h2 class="text-center text-xl font-semibold">Paste the Link</h2>
+        <!-- Card 2 -->
+        <div class="relative w-full md:w-1/3 bg-amber-50 text-black px-10 py-30 cursor-pointer rounded-3xl transform hover:scale-105 transition duration-300">
+          <div class="absolute -top-7 left-1/2 -translate-x-1/2 w-14 h-14 bg-lime-300 rounded-full flex items-center justify-center">
+            <img src=${paste} alt="Pinterest" class="w-7 h-7" />
+          </div>
+          
+          <h2 class="text-center text-xl font-semibold mt-10">Paste the Link</h2>
           <p class="mt-5 text-base font-normal">
             Go to <span class="font-bold">www.vidextract.net</span> and paste the link.
           </p>
         </div>
 
-        <div class="w-full md:w-1/3 bg-amber-50 text-black px-10 cursor-pointer rounded-3xl py-30 transform hover:scale-105 transition duration-300 ">
-          <h2 class="text-center text-xl font-semibold">Click Download</h2>
+        <!-- Card 3 -->
+        <div class="relative w-full md:w-1/3 bg-amber-50 text-black px-10 py-30 cursor-pointer rounded-3xl transform hover:scale-105 transition duration-300">
+          <div class="absolute -top-7 left-1/2 -translate-x-1/2 w-14 h-14 bg-lime-300 rounded-full flex items-center justify-center">
+            <img src=${download} alt="Pinterest" class="w-7 h-7" />
+          </div>
+          
+          <h2 class="text-center text-xl font-semibold mt-10">Click Download</h2>
           <p class="mt-5 text-base font-normal">
             Click the download button and save what you want. 
             We provide MP4 & MP3 in the highest quality — fast and free.
@@ -33,9 +53,8 @@ export function HowToDownload() {
       </div>
 
       <!-- Decorative vector light -->
-    <img src=${v1} alt="Light effect"
-           class="absolute rotate-180 -right-10 top-2/4 -translate-y-1/3 w-150 h-auto pointer-events-none select-none" />
-
+      <img src=${v1} alt="Light effect"
+        class="absolute rotate-180 -right-10 top-2/4 -translate-y-1/3 w-150 h-auto pointer-events-none select-none" />
     </div>
   `
 }
