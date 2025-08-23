b19833c466222866dab40da0b6ea0c597d8fc23f
usernamedinesh <borod9200@gmail.com>
2025-08-23 10:14:09 +0530

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 export function FooterPage() {
   const currentYear = new Date().getFullYear();
   return `
-    <footer class="relative  text-gray-300 py-10 px-6 mt-20">
+    <footer class="relative  text-gray-300 py-10 px-6">
       <div class="max-w-6xl mx-auto flex flex-col md:flex-row md:items-center md:justify-between gap-6">
-        
+
         <!-- Logo -->
         <div class="flex items-center justify-center md:justify-start gap-2">
           <span class="text-lime-300 font-bold text-2xl">VidExtract</span>
           <span class="text-white text-2xl">.net</span>
         </div>
-        
+
         <!-- Links -->
         <div class="flex justify-center gap-6 text-sm">
           <a href="#" class="hover:text-lime-300 transition">Privacy Policy</a>
