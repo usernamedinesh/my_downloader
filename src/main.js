34a7d2d7ed42e3d94721f647f4f1ba127d0c8aa8
usernamedinesh <borod9200@gmail.com>
2025-09-05 14:10:06 +0530

    fixing big of icons

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import { Router } from "./router";
 import "./css/style.css"
 
-document.querySelector('#root').innerHTML = ` 
+document.querySelector('#root').innerHTML = `
  <main id = "app" ></main>
 `;
 
 window.addEventListener('DOMContentLoaded', Router);
 
 window.addEventListener('popstate', Router)
-
 
 document.body.addEventListener('click', e => {
   if (e.target.matches('a')) {
